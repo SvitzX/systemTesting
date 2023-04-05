a356d12f98c8de57e67c0d3c06c66252f39730d9
SviridovArtem353090490004 <71284827+SviridovArtem353090490004@users.noreply.github.com>
2023-04-05 18:47:24 +0300

    add some new tests

--- main.py
+++ main.py
@@ -71,18 +71,13 @@ def test_customer_add(login_page):
     manager_dlg.Button4.click_input()
 
 def test_hire_employee(login_page):
-    login_to_account(main, credentials["admin"])
+    login_to_account(login_page, credentials["admin"])
     admin_dlg = Desktop(backend="uia").Administrator
     admin_dlg.print_control_identifiers()
-    # ввод имени
     admin_dlg.LoginEdit5.type_keys("Сергей", pause=0.05)
-    # ввод фамилии
     admin_dlg.LoginEdit4.type_keys("Спиров", pause=0.05)
-    # ввод зарплаты
     admin_dlg.LoginEdit3.type_keys(70000, pause=0.05)
-    # ввод логина
     admin_dlg.LoginEdit2.type_keys("spirov_s", pause=0.05)
-    # ввод пароля
     admin_dlg.LoginEdit1.type_keys("123", pause=0.05)
     admin_dlg.Listbox4.click_input()
     from pywinauto import mouse
@@ -95,61 +90,70 @@ def test_hire_employee(login_page):
 
     admin_dlg.Button0.click_input()
     admin_dlg.Button2.click_input()
+    assert admin_dlg.Dialog2.Static0.legacy_properties()["Name"] == "Employee created"
     admin_dlg.Dialog2.Button.click_input()
     admin_dlg.Button5.click_input()
 
+def test_analyst_check_store(login_page):
+    login_to_account(login_page, credentials["analyst"])
+    analyst_dlg = Desktop(backend="uia").Analyst
+    analyst_dlg.print_control_identifiers()
+    analyst_dlg.Button.click_input()
+    assert analyst_dlg.DataGridView.Custom2.Edit4.legacy_properties()["Value"] == "VHS Проигрыватель"
+    analyst_dlg.Button7.click_input()
 
+def test_analyst_managers_top(login_page):
+    login_to_account(login_page, credentials["analyst"])
+    analyst_dlg = Desktop(backend="uia").Analyst
+    analyst_dlg.print_control_identifiers()
+    analyst_dlg.Button2.click_input()
+    assert analyst_dlg.DataGridView.Custom2.Edit4.legacy_properties()["Value"] == "Илья"
+    analyst_dlg.Button7.click_input()
 
+def test_analyst_count_sales(login_page):
+    login_to_account(login_page, credentials["analyst"])
+    analyst_dlg = Desktop(backend="uia").Analyst
+    analyst_dlg.print_control_identifiers()
+    analyst_dlg.Button4.click_input()
+    assert analyst_dlg.DataGridView.Custom2.Edit4.legacy_properties()["Value"] == "26000"
+    analyst_dlg.Button7.click_input()
 
-
+def test_analyst_goods_top(login_page):
+    login_to_account(login_page, credentials["analyst"])
+    analyst_dlg = Desktop(backend="uia").Analyst
+    analyst_dlg.print_control_identifiers()
+    analyst_dlg.Button3.click_input()
+    assert analyst_dlg.DataGridView.Custom2.Edit4.legacy_properties()["Value"] == "DVD Проигрыватель"
+    analyst_dlg.Button7.click_input()
 
 
 if __name__ == "__main__":
-    Popen('C:\\Debug\\Course.exe')
-    main = Desktop(backend="uia").LoginPage
-    login_to_account(main, credentials["admin"])
-    admin_dlg = Desktop(backend="uia").Administrator
-    admin_dlg.print_control_identifiers()
-    # ввод имени
-    admin_dlg.LoginEdit5.type_keys("Сергей", pause=0.05)
-    # ввод фамилии
-    admin_dlg.LoginEdit4.type_keys("Спиров", pause=0.05)
-    # ввод зарплаты
-    admin_dlg.LoginEdit3.type_keys(70000, pause=0.05)
-    # ввод логина
-    admin_dlg.LoginEdit2.type_keys("spirov_s", pause=0.05)
-    # ввод пароля
-    admin_dlg.LoginEdit1.type_keys("123", pause=0.05)
-    admin_dlg.Listbox4.click_input()
-    from pywinauto import mouse
-
-    rect = admin_dlg.rectangle()
-    top, left = rect.top, rect.left
-    mouse.click(coords=(top + (280 - 130), left + (460 - 130)))
-    mouse.click(coords=(top + (280 + 350), left + (460 - 370)))
-    mouse.click(coords=(top + (280 + 450), left + (460 - 260)))
-
-    admin_dlg.Button0.click_input()
-    admin_dlg.Button2.click_input()
-    admin_dlg.Dialog2.Button.click_input()
-    admin_dlg.Button5.click_input()
-
+    # Popen('C:\\Debug\\Course.exe')
+    # main = Desktop(backend="uia").LoginPage
+    # login_to_account(main, credentials["admin"])
+    # admin_dlg = Desktop(backend="uia").Administrator
+    # admin_dlg.print_control_identifiers()
+    # admin_dlg.LoginEdit5.type_keys("Сергей", pause=0.05)
+    # admin_dlg.LoginEdit4.type_keys("Спиров", pause=0.05)
+    # admin_dlg.LoginEdit3.type_keys(70000, pause=0.05)
+    # admin_dlg.LoginEdit2.type_keys("spirov_s", pause=0.05)
+    # admin_dlg.LoginEdit1.type_keys("123", pause=0.05)
+    # admin_dlg.Listbox4.click_input()
+    # from pywinauto import mouse
+    #
+    # rect = admin_dlg.rectangle()
+    # top, left = rect.top, rect.left
+    # mouse.click(coords=(top + (280 - 130), left + (460 - 130)))
+    # mouse.click(coords=(top + (280 + 350), left + (460 - 370)))
+    # mouse.click(coords=(top + (280 + 450), left + (460 - 260)))
+    #
+    # admin_dlg.Button0.click_input()
+    # admin_dlg.Button2.click_input()
+    # assert admin_dlg.Dialog2.Static0.legacy_properties()["Name"] == "Employee created"
+    # admin_dlg.Dialog2.Button.click_input()
+    # admin_dlg.Button5.click_input()
 
     # переход к увольнению сотрудников
     #admin_dlg.TabItem2.click_input()
 
     print("")
-
-
-#for analyst (it works)
-#login_to_account(main, credentials["analyst"])
-#analyst_dlg = Desktop(backend="uia").Analyst
-#analyst_dlg.print_control_identifiers()
-#нажатие кнопки склад
-#analyst_dlg.Button.click_input()
-#нажатие кнопки топ по продажам менеджеров
-#analyst_dlg.Button2.click_input()
-#нажатие кнопки сумма продаж
-#analyst_dlg.Button4.click_input()
-#нажатие кнопки топ по товарам
-#analyst_dlg.Button3.click_input()
